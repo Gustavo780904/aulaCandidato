f08e851bdc4507e7d9a45cac07db0a5e5f3dc4e6
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-05-13 19:46:43 -0300

    Leitura da escolaridade

--- src/candidatoAula.java
+++ src/candidatoAula.java
@@ -7,8 +7,16 @@ public class candidatoAula {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int escolaridade;
 		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.print("Digite uma opção: ");
 		
+		escolaridade = sc.nextInt();
 		
 		sc.close();
 
