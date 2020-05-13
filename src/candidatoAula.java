6883b4ab0afa746d0a1c5015ee48d47cd091ac48
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-05-13 20:34:39 -0300

    Escolaridade e experiência

--- src/candidatoAula.java
+++ src/candidatoAula.java
@@ -9,6 +9,7 @@ public class candidatoAula {
 		
 		int escolaridade, experiencia, pontoExpe, pontoEscola, cargo;
 		char disponibilidade, habilitacao;
+		//leitura da escolaridade
 		System.out.println("Qual sua escolaridade?");
 		System.out.println("1) Ensino Fundamental");
 		System.out.println("2) Ensino Médio");
@@ -18,14 +19,43 @@ public class candidatoAula {
 		
 		escolaridade = sc.nextInt();
 		System.out.println();
+		// leitura da experiencia
 		System.out.print("Você tem quantos anos de experiência profissional? ");
 		experiencia = sc.nextInt();
 		System.out.print("Você tem disponibilidade para viajar (S/N)? ");
 		disponibilidade = sc.next().charAt(0);
+		// leitura da habilitacao
 		System.out.print("Você tem habilitação de motorista (S/N)? ");
 		habilitacao = sc.next().charAt(0);
-				
-				
+		System.out.println();
+		// pontuação da escolaridade
+		if (escolaridade == 1) {
+			pontoEscola = 10;
+		}
+		else if (escolaridade == 2) {
+			pontoEscola = 20;
+		}
+		else if (escolaridade == 3) {
+			pontoEscola = 30;
+		}
+		else {
+			pontoEscola = 40;
+		}
+		// pontuação da experiência
+		if(experiencia == 0) {
+			pontoExpe = 0;
+		}
+		else if (experiencia > 0 && experiencia <= 2) {
+			pontoExpe = 10;
+		}
+		else if (experiencia > 2 && experiencia <= 5) {
+			pontoExpe = 20;
+		}
+		else {
+			pontoExpe = 40;
+		}
+		System.out.println("Pontos por escolaridade: " + pontoEscola);
+		System.out.println("Pontos por experiência: " + pontoExpe);
 		sc.close();
 
 	}
